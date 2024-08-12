db7ac7d067b3b6c634ff2c708567d235476cd2bb
Kentt-O <kentc.okoro@gmail.com>
2024-08-12 05:49:28 +0100

    Tests

--- src/test/java/org/auction/services/Implementation/BidServiceImplTest.java
+++ src/test/java/org/auction/services/Implementation/BidServiceImplTest.java
@@ -1,16 +1,18 @@
 package org.auction.services.Implementation;
 
 import org.auction.DTOs.Request.BidRequest;
+import org.auction.DTOs.Response.BidResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-class BidServiceImplTest {
+public class BidServiceImplTest {
     @Autowired
     private BidServiceImpl bidService;
     @Autowired
@@ -18,8 +20,15 @@ class BidServiceImplTest {
 
     @Test
     public void customerCanPlaceBidTest(){
-        BidRequest placeBid = new BidRequest();
-        placeBid.setCustomerId("Shank");
-        placeBid.setBidAmount(new BigDecimal("30_000"));
+        BidRequest placeBidRequest = new BidRequest();
+        placeBidRequest.setCustomerId("customer001");
+        placeBidRequest.setBidAmount(new BigDecimal("100.00"));
+
+        BidResponse response = bidService.placeBid(placeBidRequest);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getBidAmount()).isEqualByComparingTo("100.00");
+        assertThat(response.getCustomerId()).isEqualTo("customer001");
+
     }
 }
